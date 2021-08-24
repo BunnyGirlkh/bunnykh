f53cf4108bba06842fe3c9a99d9a9443b33108c9
BunnyGirlkh <89449366+BunnyGirlkh@users.noreply.github.com>
2021-08-24 15:35:49 +0700

    Add files via upload

---
+++ Music/music.js
@@ -0,0 +1,22 @@
+module.exports = ({
+    name: "music",
+code: `$author[Music . ;https://media.discordapp.net/attachments/875007491715510323/875007525769076756/Music.gif] 
+
+$title[ Bunny Girl ❤️  HELP Music ❗ ]
+
+$description[**$getServerVar[prefix]play** - Play some music!.
+**$getServerVar[prefix]loopqueue** - Loops the current queue.
+**$getServerVar[prefix]nowplaying** - See the current playing song.
+**$getServerVar[prefix]pause** - Pause the current playing song.
+**$getServerVar[prefix]lyrics** - Get lyrics from the current playing or a specific song.
+**$getServerVar[prefix]resume** - Resume the current paused song.
+**$getServerVar[prefix]loopsong** - Loops the current song.
+**$getServerVar[prefix]skip** - Skips the current playing song.
+**$getServerVar[prefix]skipto** - Skip to a different song in the queue.
+**$getServerVar[prefix]stop** - Stops the current playing song.
+**$getServerVar[prefix]volume** - Change the music volume.]
+
+$color[RANDOM]
+$footer[@$username developer  |By Kakada Loveu|]
+$addTimestamp`
+})
